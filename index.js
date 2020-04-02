7924e4417365aa13aa7844478d04e6b643eb2d72
tianxiazhizhong <tianxiazhizhong@gmail.com>
2020-04-02 10:16:14 +0800

    fixed bug. before online

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
+#!/usr/bin/env node
+
 const { execSync } = require('child_process');
 const { username } = require('./lib/constant');
+const { warnLog, errLog } = require('./lib/util');
 const os = require('os');
-
-const userInfo = os.userInfo();
-if(userInfo.username !== username){
-  warnLog(`You need run command '${command}' as '${username}' user.`);
-  return;
-}
 
 const args = process.argv;
 const nodeSh = args[0];
@@ -14,6 +11,14 @@ const command = args[2];
 let params = args.slice(3);
 params = params.length ? ' ' + params.join(' ') : '';
 const cmd = `${nodeSh} ./lib/${command}.js${params}`;
+
+const userInfo = os.userInfo();
+if(userInfo.username !== username){
+  warnLog(`You need run command as '${username}' user.`);
+  return;
+}
+
+
 
 const lrCmdMap = new Map([
   ['install', true],
@@ -29,5 +34,5 @@ if(lrCmdMap.has(command)){
     cwd: __dirname
   });
 } else {
-  errLog(`Unsupported command: ${command}`);
+  errLog(`Unsupported command: '${command}'`);
 }
